f8bf708a22fab5366ce1e719fdce6a078c9abc0b
Mairis <mairiscastroflorez@gmail.com>
2021-11-04 17:15:09 -0500

    Configurando build, deploy y hosting en firebase

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save.
           <em>Hola mundo, probrando app.</em>
+          Hola Fanny!
         </p>
         <a
           className="App-link"
