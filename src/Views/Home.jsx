bd81598b3e2ec48a0b7788c9d013d6dc7f30fc4c
Camila Forero <amandaforero@outlook.com>
2021-11-09 13:57:58 -0500

    CSS home

--- src/Views/Home.jsx
+++ src/Views/Home.jsx
@@ -3,7 +3,8 @@ import '../router/App.css';
 import '../index.css'
 import React from 'react';
 import { Link } from 'react-router-dom';
-import camareros from "../media/camareros.png"
+import camareros from "../media/camareros.png";
+import chef from "../media/traje-de-chef.png"
 
 const home = () => {
   return (
@@ -11,8 +12,10 @@ const home = () => {
       <div className="App">
         <header className="App-header">
           <img src={Logo} className="App-logo" alt="logo" />
-          <button className="waiterButton"><img src={camareros} className="waiterIcon" alt="Mesero"/> <Link to='/Waiter'className="link-color"  >Mesero</Link> </button>
-          <Link to='/Chefs' className="chefButton">Cocina</Link>
+          <div>
+          <button className="waiterButton"><img src={camareros} className="icon" alt="Mesero"/> <Link to='/Waiter'className="link-color"  >Mesero</Link> </button>
+          <button className="chefButton"><img src={chef} className="icon" alt="Mesero"/><Link to='/Chefs' className="link-color">Cocina</Link></button>
+          </div>
         </header>
       </div>
     </body>
