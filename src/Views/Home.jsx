e18954810cef761c8a3aff960dca3078db48cc1b
Mairis Florez <mairiscastroflorez@gmail.com>
2021-11-11 16:23:40 -0500

    Estructurando vista home, depurando html, importando data JSON (#1)
    * aceptando cambios de pull request #2
    
    * estructurando home, importando data json

--- src/Views/Home.jsx
+++ src/Views/Home.jsx
@@ -8,17 +8,27 @@ import chef from "../media/traje-de-chef.png"
 
 const home = () => {
   return (
-    <body>
-      <div className="App">
-        <header className="App-header">
+  
+      <header className="App-header">
+        <div className="App">
           <img src={Logo} className="App-logo" alt="logo" />
           <div>
-          <button className="waiterButton"><Link to='/Waiter'className="link-color"><img src={camareros} className="icon" alt="Mesero"/> Mesero</Link> </button>
-          <button className="chefButton"><Link to='/Chefs' className="link-color"><img src={chef} className="icon" alt="Mesero"/>Cocina</Link></button>
+            <button className="waiterButton">
+              <Link to='/Waiter' className="link-color">
+                <img src={camareros} className="icon" alt="Mesero" />
+                Mesero
+              </Link>
+            </button>
+            <button className="chefButton">
+              <Link to='/Chefs' className="link-color">
+                <img src={chef} className="icon" alt="Mesero" />
+                Cocina
+              </Link>
+            </button>
           </div>
-        </header>
-      </div>
-    </body>
+        </div>
+      </header>
+
   );
 }
 export default home;
