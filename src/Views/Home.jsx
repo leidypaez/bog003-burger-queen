673b7c02ca96baa04ee005758fbda22368afc0cb
Mairis <mairiscastroflorez@gmail.com>
2021-11-11 12:32:57 -0500

    estructurando home, importando data json

--- src/Views/Home.jsx
+++ src/Views/Home.jsx
@@ -2,23 +2,33 @@ import Logo from '../media/Logo.png';
 import '../router/App.css';
 import '../index.css'
 import React from 'react';
-import  { Link } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import camareros from "../media/camareros.png";
 import chef from "../media/traje-de-chef.png"
 
 const home = () => {
   return (
-    <body>
-      <div className="App">
-        <header className="App-header">
+  
+      <header className="App-header">
+        <div className="App">
           <img src={Logo} className="App-logo" alt="logo" />
           <div>
-          <button className="waiterButton"><Link to='/Waiter'className="link-color"><img src={camareros} className="icon" alt="Mesero"/> Mesero</Link> </button>
-          <button className="chefButton"><Link to='/Chefs' className="link-color"><img src={chef} className="icon" alt="Mesero"/>Cocina</Link></button>
+            <button className="waiterButton">
+              <Link to='/Waiter' className="link-color">
+                <img src={camareros} className="icon" alt="Mesero" />
+                Mesero
+              </Link>
+            </button>
+            <button className="chefButton">
+              <Link to='/Chefs' className="link-color">
+                <img src={chef} className="icon" alt="Mesero" />
+                Cocina
+              </Link>
+            </button>
           </div>
-        </header>
-      </div>
-    </body>
+        </div>
+      </header>
+
   );
 }
 export default home;
