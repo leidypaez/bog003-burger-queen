e9e0c92c90e37a24cfb4935361b1041ca0c9ef4d
Camila Forero <amandaforero@outlook.com>
2021-11-15 11:38:37 -0500

    Header vista chefs

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,3 +1,11 @@
-import './App.css';
-import React from 'react';
-import  Link  from 'react-router-dom';
+// import '../index.css'
+// import React from 'react';
+// import  { Link }  from 'react-router-dom';
+
+// function modal(props){
+//   return(
+//      <>
+//   <h1>Pediddo entregado</h1>
+//   </>
+//   )
+// }
