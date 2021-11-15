51cd8fd38858df62c3fb3567d5c939cf300abdf2
Mairis <mairiscastroflorez@gmail.com>
2021-11-15 12:55:09 -0500

    commit

--- src/Views/Waiter.jsx
+++ src/Views/Waiter.jsx
@@ -1,10 +1,8 @@
 import '../index.css'
 import React from "react";
-import { Link } from "react-router-dom"
 import Header from "../components/header";
 import TabMenu from '../components/tabMenu';
 
-const menu = dataJson.menu;
 
 const waiter = () => {
 
